5e24197d903b1133e65ff2ae68e2f3831d393573
klionti <130088488+klionti@users.noreply.github.com>
2023-05-04 13:00:15 -0700

    Update models.py

--- service/models.py
+++ service/models.py
@@ -1,6 +1,5 @@
 """
 Models for Account
-
 All of the models are stored in this module
 """
 import logging
@@ -111,7 +110,6 @@ class Account(db.Model, PersistentBase):
     def deserialize(self, data):
         """
         Deserializes a Account from a dictionary
-
         Args:
             data (dict): A dictionary containing the resource data
         """
@@ -137,7 +135,6 @@ class Account(db.Model, PersistentBase):
     @classmethod
     def find_by_name(cls, name):
         """Returns all Accounts with the given name
-
         Args:
             name (string): the name of the Accounts you want to match
         """
